c312ec0a52656dc3858186d5d304aec9664b45e2
ShadyStokes <apollongregoras@gmail.com>
2023-07-03 21:19:05 +0300

    removed comments

--- 3d/fd3d_0.py
+++ 3d/fd3d_0.py
@@ -8,8 +8,6 @@ z50 = fd3d.run_sim()
 fig, ax = plt.subplots(figsize = (5,5))
 ax.set_xlabel('y')
 ax.set_ylabel('x')
-# title = fig.text(0.5, 0.92, "")
-# fig.tight_layout()
 fig.subplots_adjust(
     bottom = 0.1, top = 0.9, 
     left = 0.1, right = 0.9
@@ -27,14 +25,6 @@ for frame in range(z50.shape[0]):
     if frame % 10 == 0:
         print(f'drawing frame {frame}')
 
-    # plt.subplots_adjust(
-    #     bottom = 0.1, top = 0.9, 
-    #     left = 0.1, right = 0.8
-    # )
-    # title.set_text(f'E_z(z = 50), step = {(frame + 1) * 2}')
-    # fig.text(0.5, 0.92, f'E_z(z = 50), step = {(frame + 1) * 2}')
-    # ax.set_title(f'E_z(z = 50), step = {(frame + 1) * 2}')
-
     ax.contour(
         z50[frame,:,:], **contour_params,
         colors = 'black', linewidths = 0.5, linestyles = 'solid'
@@ -42,8 +32,6 @@ for frame in range(z50.shape[0]):
     ax.contourf(
         z50[frame,:,:], **contour_params
     )
-
-    # plt.colorbar(location = 'right', orientation = 'vertical', cax = ax)
 
     ax.vlines([40, 70], 0, 99, color = 'black')
     
